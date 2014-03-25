fc67376ab3f195664ad2525a99df6bbcaffd0d1d
Jan Antala <jan@antala.sk>
2014-03-25 15:23:50 +0100

    test: add tests

--- test/unit.spec.js
+++ test/unit.spec.js
@@ -1,9 +1,34 @@
 'use strict';
 
-describe('unit specs', function() {
+describe('adaptive.youtube', function() {
 
-  it('should do something', function() {
+  beforeEach(module('adaptive.youtube'));
 
+  var rootScope;
+  beforeEach(inject(function($rootScope) {
+    rootScope = $rootScope;
+  }));
+
+  var elm, scope, ctrl, createController;
+
+  beforeEach(inject(function($rootScope, $compile) {
+    scope = $rootScope.$new();
+    scope.youtubeId = 'kxopViU98Xo'; 
+
+    elm = angular.element(
+      '<youtube video="youtubeId" class="youtube"></youtube>'
+    );
+
+    $compile(elm)(scope);
+    scope.$digest();
+  }));
+
+  it('shold have correct video id', function(){
+    expect(scope.youtubeId).toBe('kxopViU98Xo');
   });
-  
+
+  it('shold have background image', function(){
+    expect(elm.css('background-image').match(/\/\/img.youtube.com\/vi\/kxopViU98Xo\/0.jpg/g).length).toBe(1);
+  });
+
 });
